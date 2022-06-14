b83ede4a57e5c4f0f8c755a698c9d0bb8d2e6161
dmitriy_martys <12masek34@mail.ru>
2022-06-14 18:41:21 +0300

    add functional test in unitTest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000/')
 
-assert 'Title' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    """Test new visit"""
+
+    def setUp(self) -> None:
+        """install"""
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        """uninstall"""
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """Test todo list"""
+        self.browser.get('http://localhost:8000/')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('End test')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
