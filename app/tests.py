e40ca5d387f95106162c261a9f8c03f3c3c04a18
dmitriy_martys <12masek34@mail.ru>
2022-06-15 12:52:55 +0300

    add test index page return html

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,22 @@
+from django.http import HttpRequest
+from django.urls import resolve
 from django.test import TestCase
+from app.views import index
 
-# Create your tests here.
+
+class IndexPageText(TestCase):
+    """Test home page"""
+
+    def test_root_url_resolves_to_home_page_view(self):
+        """Test source urls in index page"""
+        found = resolve('/')
+        self.assertEqual(found.func, index)
+
+    def test_home_page_returns_correct_html(self):
+        """test corrct returns html """
+        request = HttpRequest()
+        response = index(request)
+        html = response.content.decode('utf8')
+        self.assertTrue(html.startswith('<!DOCTYPE html>'))
+        self.assertIn('<title>To-Do lists</title>', html)
+        self.assertTrue(html.endswith('</html>'))
--- app/views.py
+++ app/views.py
@@ -4,8 +4,5 @@ from .models import Blog
 
 
 def index(request):
-    user = User.objects.get(pk=2)
-    blogs = Blog.objects.first()
-    # print(user.blog_set.all())
     context = {}
     return render(request, 'app/index.html', context)
